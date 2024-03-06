0af8d606d8c6d770c67e8bc7eee2de708a6516f6
simone-danzeri <s.danzeri@icloud.com>
2024-03-06 15:09:34 +0100

    feat: add milestone 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,68 @@
-// Chiedere quanti km da fare
-// Chiedere età
-// Calcolare eventuale sconto
-// Applicare sconto
 // pulsante che calcola prezzo biglietto
+
+let priceButton = document.querySelector('#ticket-price-calc');
+priceButton.addEventListener('click', function () {
+// Chiedere quanti km da fare e trasformarli in num
+const userKm = document.querySelector('#user-km').value;
+let userKmAsNumeric = parseInt(userKm);
+
+console.log('Km da fare', userKmAsNumeric);
+
+// Chiedere età e trasformarla in num
+const userAge = document.querySelector('#user-age').value;
+let userAgeAsNumeric = parseInt(userAge);
+
+console.log('Età', userAgeAsNumeric);
+
+// Calcolare prezzo biglietto
+const ticketFare = 0.21;
+let ticketPrice = ticketFare * userKmAsNumeric;
+
+console.log('Prezzo base biglietto', ticketPrice);
+
+// Calcolare eventuale sconto
+let underageDiscount;
+let elderlyDiscount;
+
+if (userAge < 18) {
+
+    underageDiscount = ticketPrice * 20 /100;
+
+} else if (userAge > 65) {
+
+    elderlyDiscount = ticketPrice * 40 / 100;
+    
+}
+
+console.log('Valore sconto se minorenne', underageDiscount);
+console.log('Valore sconto se over 65', elderlyDiscount);
+
+// Applicare sconto
+let underagePrice;
+let elderlyPrice;
+
+underagePrice = (ticketPrice - underageDiscount).toFixed(2);
+elderlyPrice = (ticketPrice - elderlyDiscount).toFixed(2);
+
+console.log('Prezzo biglietto se minorenne', underagePrice);
+console.log('Prezzo biglietto se over 65', elderlyPrice);
+
+// Stampare output prezzo finale
+    let finalPrice
+
+    if (userAge < 18) {
+
+        finalPrice = 'Il costo del tuo biglietto è: ' + underagePrice;
+
+    } else if (userAge > 65) {
+
+        finalPrice = 'Il costo del tuo biglietto è: ' + elderlyPrice;
+
+    } else {
+
+        finalPrice = 'Il costo del tuo biglietto è: ' + ticketPrice;
+
+    }
+
+    console.log(finalPrice);
+});
